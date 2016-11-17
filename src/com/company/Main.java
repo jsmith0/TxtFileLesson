649ab41004f2c16cc2e30128656eb754b3e56020
js456 <justins4@comcast.net>
2016-11-17 10:21:42 -0500

    lesson

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
 	    Scanner sf = new Scanner(new File("MyData2.txt"));
 
+        int maxIndex = -1;
+        String text [] = new String [1000];
+        while (sf.hasNext())
+        {
+            maxIndex++;
+            text[maxIndex] = sf.nextLine();
+        }
         sf.close();
+        for(int j = 0; j <= maxIndex; j++){
+            System.out.println(text[j]);
+        }
     }
 }
