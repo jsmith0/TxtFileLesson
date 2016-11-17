948144d925b8473af472c55b90f21f8c88b538ff
js456 <justins4@comcast.net>
2016-11-17 10:16:46 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    Scanner sf = new Scanner(new File("MyData2.txt"));
+
+        sf.close();
+    }
+}
